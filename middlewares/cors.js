b66c2456702b0bcb2b4edde26d8df241e53d9b8f
Zh0r1k <ZhonamMax@gmail.com>
2024-05-28 18:59:25 +0300

    dfdfdfdfdfdfdfdfdfdfdfdfdf

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,15 +1,17 @@
-const allowedCors = [
-  'https://frontend-zhuravlevilya.nomoredomainswork.ru',
-  'http://frontend-zhuravlevilya.nomoredomainswork.ru',
-  'http://localhost:3000'
-];
+const allowedCors = ["http://localhost:3000"];
+
 function cors(req, res, next) {
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)){
-      res.header('Access-Control-Allow-Origin', origin);
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
   }
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-  next()
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+  );
+  next();
 }
-module.exports = {cors} 
+
+module.exports = cors;
