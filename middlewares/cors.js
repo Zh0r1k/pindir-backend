1c45770b86f5a8f30ff54cb5c885e7a1309db29b
Zh0r1k <ZhonamMax@gmail.com>
2024-05-28 18:57:07 +0300

    ghgggghghghg

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -12,6 +12,4 @@ function cors(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next()
 }
-module.exports = {
-  cors
-} 
+module.exports = {cors} 
